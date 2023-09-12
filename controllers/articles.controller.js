5e4b269de9d3f762c9b5d1c39a8449f6f77c377e
Ahmed <ahmedaraby01023980886@gmail.com>
2023-09-12 21:24:44 +0300

    v1.0

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,8 +1,37 @@
 const { articles } = require("../models/articles.model")
 
 exports.getAllArticles = (req, res) => {
+    if (!req.query.page) {
+        req.query.page = 1;
+    }
     articles.find({}).then(result => {
-        res.send(result)
+        const perPage = 9;
+        const pages = Math.ceil(result.length / perPage);
+        const from = (req.query.page - 1) * perPage;
+        const to = (req.query.page - 1) * perPage + perPage;
+        let response = {
+            data: result.filter((i, index) => index >= from && index < to),
+            total: result.length,
+            perPage
+        }
+        let links = [
+            {
+                url: req.query.page != pages ? `${req.baseUrl}?page=${Number(req.query.page) + 1}` : null
+            },
+            {
+                url: req.query.page != 1 ? `${req.baseUrl}?page=${req.query.page - 1}` : null
+            }
+        ];
+        for (let i = 1; i <= pages; i++) {
+            const element = {
+                url: `${req.baseUrl}?page=${i}`,
+                label: i,
+                active: req.query.page == i
+            }
+            links.push(element);
+        }
+        response.links = links;
+        res.send(response)
     })
 }
 

3f40d5fe8adcab6ea419e9b0f54d0cbd5dbd828b
Ahmed <ahmedaraby01023980886@gmail.com>
2023-11-06 22:26:05 +0200

    admin control

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -28,3 +28,76 @@ exports.login = (req, res) => {
         res.sendStatus(400)
     }
 }
+
+exports.createAdmin = (req, res) => {
+    const { username, password, secret } = req.body;
+    if (username && password) {
+        if (secret == process.env.CREATE_ADMIN) {
+            admins.findOne({ username }).then(user => {
+                if (!user) {
+                    bcrypt.hash(password, 12).then(hash => {
+                        admins.create({ username, password: hash }).then(result => {
+                            res.status(201).send(result)
+                        })
+                    })
+                } else {
+                    res.sendStatus(409)
+                }
+            })
+        } else {
+            res.sendStatus(401)
+        }
+    } else {
+        res.sendStatus(400)
+    }
+}
+
+exports.updateAdmin = (req, res) => {
+    const { _id } = req.params;
+    const { username, oldPass, newPass } = req.body;
+    admins.findOne({ _id }).then(user => {
+        if (user) {
+            if (oldPass) {
+                bcrypt.compare(oldPass, user.password).then(check => {
+                    if (check) {
+                        if (username) {
+                            admins.findOne({ username }).then(dup => {
+                                if (!dup || dup._id == _id) {
+                                    bcrypt.hash(newPass, 12).then(hash => {
+                                        admins.findByIdAndUpdate({ _id }, { username, password: hash }, { new: true }).then(result => {
+                                            res.status(201).send(result)
+                                        })
+                                    })
+                                } else {
+                                    res.sendStatus(409)
+                                }
+                            })
+                        } else {
+                            bcrypt.hash(newPass, 12).then(hash => {
+                                admins.findByIdAndUpdate({ _id }, { password: hash }, { new: true }).then(result => {
+                                    res.status(201).send(result)
+                                })
+                            })
+                        }
+                    } else {
+                        res.sendStatus(401)
+                    }
+                })
+            } else if (username) {
+                admins.findOne({ username }).then(dup => {
+                    if (!dup || dup._id == _id) {
+                        admins.findByIdAndUpdate({ _id }, { username }, { new: true }).then(result => {
+                            res.status(201).send(result)
+                        })
+                    } else {
+                        res.sendStatus(409)
+                    }
+                })
+            } else {
+                res.send(200)
+            }
+        } else {
+            res.sendStatus(404)
+        }
+    })
+}

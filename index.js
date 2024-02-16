c9a83a357ac5afddc6bf773eefd3ac811e4d3afc
Ahmed <ahmedaraby01023980886@gmail.com>
2024-02-16 18:59:12 +0200

    about

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use("/articles", require('./routes/articles.route'));
 app.use("/admin", require('./routes/admin.route'));
 app.use("/social", require('./routes/social.route'));
 app.use("/tags", require('./routes/tags.route'));
+app.use("/BOD", require('./routes/BOD.route'));
+app.use("/about", require('./routes/about.route'));
 
 connect().then(() => {
     app.listen(8000, () => {
